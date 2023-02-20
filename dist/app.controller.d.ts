73201c84a4dbf6c5c765beaaef04551cea4a829b
JovanniF <kapitaldev2@kapital.mx>
2023-02-20 12:08:55 -0600

    first commit

--- dist/app.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { ClientProxy } from '@nestjs/microservices';
-import { AppService } from './app.service';
-export declare class AppController {
-    private readonly appService;
-    private readonly kakfa;
-    constructor(appService: AppService, kakfa: ClientProxy);
-    messageCreate(payload: any): void;
-    send_message(message: any): Promise<any>;
-}
